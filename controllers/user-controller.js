8abd57832f4ae4486343bcb16dc2a368f4566421
nbross <nbross2001@gmail.com>
2022-06-27 22:22:27 -0400

    problem with controllers again

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 
 const userController = {
     // get all users /api/users
@@ -16,22 +16,22 @@ const userController = {
     // get user by id /api/users/:id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-            .populate({
-                path: "thoughts",
-                select: "-__v",
-            })
-            .select("-__v")
-            .then((dbUserData) => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: "No user found with this ID" });
-                    return;
-                }
-                res.json(dbUserData);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(400).json(err);
-            });
+        .populate([
+            { path: 'thoughts', select: "-__v" },
+            { path: 'friends', select: "-__v" }
+        ])
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
 
     // create a user /api/users
